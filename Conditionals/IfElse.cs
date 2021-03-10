32ddf331276aa7eae5229cd6b464712a7ae7eec3
jeromedawn <jeromedawntraining@gmail.com>
2021-03-10 12:03:05 -0500

    Conditionals

---
+++ Conditionals/IfElse.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Conditionals
+{
+    [TestClass]
+    public class IfElse
+    {
+        [TestMethod]
+        public void IfStatements()
+        {
+            bool userIsHungry = true;
+            if (userIsHungry)
+            {
+                Console.WriteLine("Eat Something");
+
+            }
+
+            int hoursStudying = 1;
+            if (hoursStudying < 16) ;
+            {
+                Console.WriteLine("You're not trying");
+            }
+        }
+        [TestMethod]
+
+        public void IfElseStatements()
+        {
+            bool choresAreDone = false;
+             if (choresAreDone)
+            {
+                Console.WriteLine("Have fun");
+
+            }
+             else
+            {
+                Console.WriteLine("Stay home");
+            }
+
+            // Turning String into Int 
+
+            string input = "2";
+            int totalHours = int.Parse(input);
+
+            if(totalHours >= 8)
+            {
+                Console.WriteLine("Well Rested");
+            }
+            else
+            {
+                Console.WriteLine("Gonna be tired");
+                if(totalHours< 4 )
+                Console.WriteLine("Get more sleep");
+            }
+
+            int age = 32;
+            if (age > 17)
+            {
+                Console.WriteLine("adult");
+            }
+            else
+            {
+                if (age > 6)
+                {
+                    Console.WriteLine("Kid");
+                }
+                else if (age > 0)
+                {
+                    Console.WriteLine("too young for PC");
+                }
+                else
+                {
+                    Console.WriteLine("You're not born yet");
+                }
+            }
+
+            if(age <65 && age >18)
+            {
+                Console.WriteLine("Age is between 18 and 65");
+            }
+
+            if (age > 65 || age < 18)
+            {
+                Console.WriteLine("Age is greater than 65 or less than 18");
+            }
+
+            if (age  == 21)
+            {
+                Console.WriteLine("Age is equal to 21");
+            }
+
+            if (age != 36)
+            {
+                Console.WriteLine("Age is not equal to 36");
+            }
+
+
+        }
+    }
+}
---
+++ Conditionals/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Conditionals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Conditionals")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef3d1942-ec4d-42e9-acf6-57f6c4a50e02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
